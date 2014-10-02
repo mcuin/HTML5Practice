bae5c0bfd87ca7597787eb245144158947e4b92a
Mykal Cuin <mykcuin@gmail.com>
2014-10-02 14:09:21 -0600

    Finished geolocation.

--- GeoLocation/geoscript.js
+++ GeoLocation/geoscript.js
@@ -1,4 +1,4 @@
-var x = document.getById("geoDemo");
+var x = document.getElementById("geoDemo");
 
 function geoLocate() {
 	if (navigator.geolocation) {
@@ -10,5 +10,25 @@ function geoLocate() {
 
 function showPosition(position) {
 	x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+
+	var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
+
+	document.getElementById("mapholder").innerHTML = "<img src='"+img_url"'>";
 } 
- 
+
+function showError(error) {
+	switch(code.error) {
+		case error.PERMISSION_DENIED:
+			x.innerHTML = "User denied the request for geolocation."
+			break;
+		case error.POSITION_UNAVAILABLE:
+			x.innerHTML = "Location information is unavailable."
+			break;
+		case error.TIMEOUT:
+			x.innerHTML = "The request to get user location timed out."
+			break;
+		case error.UNKNOWN_ERROR:
+			x.innerHTML = "An unknown error occured."
+			break;
+	}
+} 
